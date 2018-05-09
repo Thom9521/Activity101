228f0fb17e74d7bfc464f7f9c552225697c28a32
Thom9521 <Thom9521@edu.easj.dk>
2018-05-09 11:28:09 +0200

    Fixed crash

--- app/src/main/java/com/jfdimarzio/activity101/MainActivity.java
+++ app/src/main/java/com/jfdimarzio/activity101/MainActivity.java
@@ -5,6 +5,11 @@ import android.app.ProgressDialog;
 import android.os.CountDownTimer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 
 public class MainActivity extends Activity {
@@ -18,6 +23,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         Log.d(tag, "Kører onCreate()");
 
+        Button buttonP = (Button) findViewById(R.id.buttonP);
+        buttonP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView tw = (TextView) findViewById(R.id.textView);
+                tw.setText("No crasharino feelsamazingman");
+            }
+        });
     }
 
     public void onStart() {
@@ -36,6 +49,10 @@ public class MainActivity extends Activity {
             }
         }.start();
     }
+
+
+
+
 
     public void onRestart() {
         super.onRestart();
